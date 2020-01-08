0878d050fe2135980bcf120c683848c6239b028c
marcocola01 <colangelo.marco@iisgalvanimi.edu.it>
2020-01-08 10:34:27 +0000

    punto 1 completo

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var router = express.Router();
-const sql = require('mssql');
-
+const sql = require('mssql')
 const config = {
   user: '4DD_09',  //Vostro user name
   password: 'xxx123##', //Vostra password
-  server: "213.140.22.237\\sqlexpress",  //Stringa di connessione
-  database: 'wtSchool', //(Nome del DB)
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_09', //(Nome del DB)
 }
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/attori', function(req, res, next) {
   sql.connect(config, err => {
     if(err) console.log(err);  // ... error check
     
     // Query
     let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from school.Person', (err, result) => {
+    sqlRequest.query('select * from DATIIMPIEGATO', (err, result) => {
         if (err) console.log(err); // ... error checks
         res.send(result);  //Invio il risultato
     });

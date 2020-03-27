c56585cc34e254973513c76692af94bf8957891d
marcocola01 <colangelo.marco@iisgalvanimi.edu.it>
2020-03-27 10:45:12 +0000

    es 06.4 completo

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -19,7 +19,7 @@ postData : Object;
   constructor(private http: HttpClient) { }
   getUnitList(): void {
     //Qui va sostituito l’url con quello delle vostre api
-    this.obsUnit = this.http.get<Unit[]>('http://localhost:3000/users');
+    this.obsUnit = this.http.get<Unit[]>('https://3000-a584f0bd-88b6-4ba6-a9ba-53d156772d3a.ws-eu01.gitpod.io/');
     //Mi sottoscrivo all’observable e scrivo la arrow function che riceve i dati
     this.obsUnit.subscribe((data: Unit[]) => {this.data = data;});
   }
